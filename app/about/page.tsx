e9013d478b2f9175e1ccb959887efc0c26d1131a
Hassanjaved17 <hassanaptech9@gmail.com>
2026-07-06 14:03:09 +0500

    Enhance about and contact pages with structured layout and styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const page   = () => {
   return (
-    <div>about page</div>
+    <div className='main backdrop:blur-sm bg-white/30 rounded-lg p-4 shadow-md'>
+<div>
+    <h1>about page</h1>
+    <p>This is the about page.</p>
+    
+</div>
+
+    </div>
   )
 }
 
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      contact page
+    <div className='main backdrop:blur-sm bg-white/30 rounded-lg p-4 shadow-md'>
+      <h1 className=' text-2xl font-bold stroke-yellow-300 outline-amber-300'>Contact Page</h1>
+      <p className=' text-lg '>This is the contact page.</p>
     </div>
   )
 }
