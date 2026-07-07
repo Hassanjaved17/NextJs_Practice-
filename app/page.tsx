27d132c1bb7d3217db08c2d02ed4f28acc563c72
Hassanjaved17 <hassanaptech9@gmail.com>
2026-07-07 18:28:37 +0500

    "practice app router in nextjs"

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      hello next
+    <div>  
+      hello next js 
     </div>
   )
 }
+
+// in NextJs app folder is the entry point of the application, so this page.tsx file is the main page of the application.
 
 export default page
