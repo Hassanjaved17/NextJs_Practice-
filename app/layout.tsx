7bd48bfbbe573beb48029c4a0746dc65c864023f
Hassanjaved17 <hassanaptech9@gmail.com>
2026-08-24 21:46:13 +0500

    Add Navbar component and integrate it into the RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/nav/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Navbar />
                 <h2>🌍 Root Layout</h2>        
         {children}</body>
     </html>
